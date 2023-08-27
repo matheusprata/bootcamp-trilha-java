95e5437483183de2a9eb8e975fca973026d936f9
matheusprata <matheusprata.ti@gmail.com>
2023-08-26 23:29:37 -0300

    Renomear classe main e implementado o TODO organizando o que vai ser feito no projeto

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,8 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //TODO: Exibir a mensagem para o usuario solicitando os dados.
+        //TODO: Obter atraves do scanner os valores digitados no terminal.
+        //TODO: Exibir a mensagem conta criada.
+    }
+}
